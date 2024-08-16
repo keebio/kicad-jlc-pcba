4b55d337f3fd3d7fcce7c0e53588d1a442533265
Danny Nguyen <danny@keeb.io>
2024-08-16 12:00:11 -0400

    Add more alignment fixes

--- make_jlc_cpl.fsx
+++ make_jlc_cpl.fsx
@@ -41,6 +41,9 @@ let absXY (r: KicadPos.Row) =
 let absX (r: KicadPos.Row) =
     KicadPos.Row(r.Ref, r.Val, r.Package, abs (r.PosX), r.PosY, r.Rot, r.Side)
 
+let flipRotation (r: KicadPos.Row) =
+    KicadPos.Row(r.Ref, r.Val, r.Package, r.PosX, r.PosY, -r.Rot, r.Side)
+
 let rotate (degrees: decimal) (row: KicadPos.Row) =
     let newRot = (row.Rot + degrees) % 360m
     KicadPos.Row(row.Ref, row.Val, row.Package, row.PosX, row.PosY, newRot, row.Side)
@@ -61,6 +64,7 @@ let fixRotations (row: KicadPos.Row) =
     | r when r.Package.Contains "Hotswap" -> (rotate 180m >> offset 0.635m -3.81m) r
     //| r when r.Package.Contains "HRO-TYPE-C" -> (rotate 180m >> offset 0m 5.0m) r
     | r when r.Package = "USB_C_Receptacle_HRO_TYPE-C-31-M-12" -> (rotate 180m >> offset 0m 1.25m) r
+    | r when r.Package = "USB_C_Receptacle_TYPE-C-16P-CB1-6-073" -> (flipRotation >> rotate 180m >> offset 0m -1.8m) r
     | r when r.Package = "SOT-23" -> rotate 180m r
     | r when r.Package = "SOIC-8_5.23mm-USON-8_2x3mm" -> rotate 90m r
     | r when r.Package.Contains "SOIC-8" -> rotate -90m r
@@ -70,6 +74,10 @@ let fixRotations (row: KicadPos.Row) =
     | r when r.Package = "QFN-48-1EP_7x7mm_P0.5mm_EP5.6x5.6mm" -> rotate -90m r
     | r when r.Package = "WS2812B" -> rotate 90m r
     | r when r.Package = "RP2040-QFN-56" -> rotate 180m r
+    | r when r.Package = "SW_choc_v2_HS_CPG135001S30_1u" -> (flipRotation >> offset 2.5m 4.85m) r
+    | r when r.Package = "SW_choc_v1+v2_HS_CPG135001S30_1u" -> (flipRotation >> offset 2.5m 4.85m) r
+    | r when r.Package = "SK6812-MINI-E" -> flipRotation r
+    //| r when r.Val = "2N7002" -> rotate 180m r
     | _ -> row
 
 let convertPos (inputCsv: string) (outputCsv: string) =
